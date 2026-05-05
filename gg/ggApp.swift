02610871a96beefdf8b4c858ac6ed0ccee8db9f5
Alperen Kişi <alperenkisi@teknasyon.com>
2026-05-05 20:56:15 +0300

    Initial Commit

---
+++ gg/ggApp.swift
@@ -0,0 +1,17 @@
+//
+//  ggApp.swift
+//  gg
+//
+//  Created by AlperenKişi on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
